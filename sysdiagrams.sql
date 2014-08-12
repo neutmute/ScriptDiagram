cc361359614bd0444221309cd7891e4384f2a3d1
NM <neutmute@neutmute.nm>
2014-08-12 11:33:46 +1000

    add drop property script

--- sysdiagrams.sql
+++ sysdiagrams.sql
@@ -25,6 +25,15 @@ GO
 /*
 If the 'microsoft_database_tools_support' property already exists on the target, it needs to be deleted otherwise SSDT will try and publish the table which already exists
 The side effect is the table is no longer hidden under 'System Tables' in SSMS
+
+Use this script to remove the property:
+
+IF EXISTS (SELECT 1 FROM SYS.EXTENDED_PROPERTIES WHERE [major_id] = OBJECT_ID('sysdiagrams') AND [name] = N'microsoft_database_tools_support' AND [minor_id] = 0)
+BEGIN
+	PRINT 'Dropping sysdiagrams extended property'
+	EXEC sys.sp_dropextendedproperty @name=N'microsoft_database_tools_support' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'sysdiagrams'
+END
+
 */
 --EXEC sys.sp_addextendedproperty @name=N'microsoft_database_tools_support', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'sysdiagrams'
 --GO
